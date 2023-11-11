519466775988afe7d1807ff81d3d3efe3992756c
tylermjensen <tylermatthewjensen@gmail.com>
2023-11-10 22:20:13 -0700

    edits

---
+++ fpr/slideshow.js
@@ -0,0 +1,27 @@
+// Add this to a new file called slideshow.js or include it in your existing script file
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
